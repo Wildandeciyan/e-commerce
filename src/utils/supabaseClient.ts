5543e9e218f477b6b69d246a32839537b2e94e6c
wildan73 <wildan.10122194@mahasiswa.unikom.ac.id>
2025-07-19 20:44:43 +0700

    update UI homepage

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 // src/utils/supabaseClient.ts
+=======
+>>>>>>> 7e00ed4 (Initial commit for e-commerce app)
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
+<<<<<<< HEAD
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+=======
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+>>>>>>> 7e00ed4 (Initial commit for e-commerce app)
